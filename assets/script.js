e75c506c03aa82273983f83d2fc4c0be63fc49d7
Steffan Carter <xisled09@gmail.com>
2022-03-25 18:10:56 -0400

    Added check boxes and prompt feature

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,11 @@
-
+var tesT = [2];
 
 function myFunction(){
-    document.getElementById("pass").innerHTML = "Please choose the following";
+    document.getElementById("pass").innerHTML = "Select two of the following:";
+    document.getElementById("section1").style.visibility="visible";
+    document.getElementById("section1").className = 'show';
+}
+
+function myTest() {
+    let person = prompt("Password is ", tesT);
 }
