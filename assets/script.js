80e927bcf297052d57955836ed811254962a9921
Steffan Carter <xisled09@gmail.com>
2022-03-26 21:42:35 -0400

    Did what I could

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,14 @@
-var tesT = [2];
+// promp variable
+var tesT = "I tried, tutor time!";
 
+// function for button
 function myFunction(){
     document.getElementById("pass").innerHTML = "Select two of the following:";
     document.getElementById("section1").style.visibility="visible";
     document.getElementById("section1").className = 'show';
 }
 
+// prompt function
 function myTest() {
     let person = prompt("Password is ", tesT);
 }
